4d433cdb7d0f441162a9aa9030c255d694b58877
Ryan Hardoon <ryan.hardoon@hp.com>
2015-06-02 10:54:41 +0300

    adding tests

--- test/aws_ec2.js
+++ test/aws_ec2.js
@@ -26,6 +26,7 @@ if (execCloudTests !== 'true') {
    return;
 }
 
+
 describe('checking aws-ec2 atomic lib', function() {
 
    var g_node = '',
@@ -133,7 +134,7 @@ describe('checking aws-ec2 atomic lib', function() {
       });
    });
 
-   it('should create image from a node on aws-ec2', function(done) {
+   it.only('should create image from a node on aws-ec2', function(done) {
       var settings = {
          regionContext: regionContext,
          imageParams: {
@@ -276,7 +277,7 @@ describe('checking aws-ec2 atomic lib', function() {
       });
    });
 
-   it.only('check launch permission api of image', function(done) {
+   it('check launch permission api of image', function(done) {
       var settings = {
          regionContext: regionContext,
          imageId: 'ami-bca4a8d4',  //a special image (plain ubuntu)created in advance for unit tests
@@ -333,43 +334,51 @@ describe('checking aws-ec2 atomic lib', function() {
 
    it('check credential validation', function(done) {
       var settings = {
-         regionContext: ec2.createRegionContext(awsEast1Settings)
+         accountId :   awsEast1Settings.accountId,
+         credentials: {
+            "accessKeyId": awsEast1Settings.accessKey,
+            "secretAccessKey": awsEast1Settings.secretKey
+
+         }
       };
       this.timeout(10000);
-      ec2.getAccountId(settings, function(error, result) {
+      ec2.validateCredentials(settings, function(error, result) {
          should.not.exist(error);
-         result.accountId.length.should.be.equal(12);
+         result.should.be.equal(0); //0 means validation success
          done();
       });
    });
 
-   it.only('check invalid credential validation', function(done) {
+   it('check mismatch between account Id and security credentials', function(done) {
       var settings = {
-         regionContext: ec2.createRegionContext({
-            "accessKey": "AKIAJSAFYZITCGZGBL3Q",
-            "secretKey": "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",
-            "region": "us-east-1"
-         })
+         accountId :   '123456789012', //give a different account
+         credentials: {
+            "accessKeyId": awsEast1Settings.accessKey,
+            "secretAccessKey": awsEast1Settings.secretKey
+
+         }
       };
       this.timeout(10000);
-      ec2.getAccountId(settings, function(error) {
-         should.exist(error);
+      ec2.validateCredentials(settings, function(error, result) {
+         should.not.exist(error);
+         result.should.be.equal(1); //credentials Ok but account does not .
          done();
       });
    });
 
-   //errors other than valid credentils (e.g. wrong region etc)
-   it('check other error credential validation', function(done) {
+   it('check invalid security credentials', function(done) {
       var settings = {
-         regionContext: ec2.createRegionContext({
-            "accessKey": "Dummy Access Key",
-            "secretKey": "Dummy Secret Key",
-            "region": "dummy region"
-         })
+         accountId :   awsEast1Settings.accountId,
+         credentials: {
+            "accessKeyId": 'ABCDEFGHIJ1234567890',
+            "secretAccessKey": awsEast1Settings.secretKey
+
+         }
       };
       this.timeout(10000);
-      ec2.getAccountId(settings, function(error) {
-         should.exist(error);
+      ec2.validateCredentials(settings, function(error, result) {
+         should.not.exist(error);
+         result.should.be.equal(2); //2 means security credentials are wrong
          done();
       });
    });
